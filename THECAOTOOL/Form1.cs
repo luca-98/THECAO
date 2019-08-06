259ddbaf3ac44926598e2deee23f8fb074fc8b0c
NguyenDucThien98 <NguyenDucThien98@users.noreply.github.com>
2019-08-06 22:12:42 +0700

    update save

--- THECAOTOOL/Form1.cs
+++ THECAOTOOL/Form1.cs
@@ -255,34 +255,33 @@ namespace THECAOTOOL {
 
 
         private void Button2_Click(object sender,EventArgs e) {
-            // creating Excel Application  
-            Microsoft.Office.Interop.Excel._Application app = new Microsoft.Office.Interop.Excel.Application();
-            // creating new WorkBook within Excel application  
-            Microsoft.Office.Interop.Excel._Workbook workbook = app.Workbooks.Add(Type.Missing);
-            // creating new Excelsheet in workbook  
-            Microsoft.Office.Interop.Excel._Worksheet worksheet = null;
-            // see the excel sheet behind the program  
-            app.Visible = true;
-            // get the reference of first sheet. By default its name is Sheet1.  
-            // store its reference to worksheet  
-            worksheet = workbook.Sheets["Sheet1"];
-            worksheet = workbook.ActiveSheet;
-            // changing the name of active sheet  
-            worksheet.Name = "1";
-            // storing header part in Excel  
-            for (int i = 1; i < dataGridView1.Columns.Count + 1; i++) {
-                worksheet.Cells[1,i] = dataGridView1.Columns[i - 1].HeaderText;
-            }
-            // storing Each row and column value to excel sheet  
-            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++) {
-                for (int j = 0; j < dataGridView1.Columns.Count; j++) {
-                    worksheet.Cells[i + 2,j + 1] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.InitialDirectory = @"C:\'";
+            saveFileDialog1.Title = "Save excel Files";
+            saveFileDialog1.DefaultExt = "xlsx";
+            saveFileDialog1.Filter = "Text files (*.xlsx)|*.xlsx";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK) {
+                Microsoft.Office.Interop.Excel._Application app = new Microsoft.Office.Interop.Excel.Application();
+                Microsoft.Office.Interop.Excel._Workbook workbook = app.Workbooks.Add(Type.Missing);
+                Microsoft.Office.Interop.Excel._Worksheet worksheet = null;
+                app.Visible = true;
+                worksheet = workbook.Sheets["Sheet1"];
+                worksheet = workbook.ActiveSheet;
+                worksheet.Name = "1";
+                for (int i = 1; i < dataGridView1.Columns.Count + 1; i++) {
+                    worksheet.Cells[1,i] = dataGridView1.Columns[i - 1].HeaderText;
                 }
+                for (int i = 0; i < dataGridView1.Rows.Count - 1; i++) {
+                    for (int j = 0; j < dataGridView1.Columns.Count; j++) {
+                        worksheet.Cells[i + 2,j + 1] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+                    }
+                }
+                workbook.SaveAs(saveFileDialog1.FileName);
             }
-            // save the application  
-            workbook.SaveAs("output.xls",Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive);
-            // Exit from the application  
-            //app.Quit();
+
         }
 
         private void DataGridView1_CellClick(object sender,DataGridViewCellEventArgs e) {
